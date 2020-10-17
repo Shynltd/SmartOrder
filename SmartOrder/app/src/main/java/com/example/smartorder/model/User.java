072341f6a29800c20725c9a9b363fba52b96274b
missxitiin123@gmail.com <Daicaptain123>
2020-10-18 02:20:09 +0700

    test login retrofit with localhost

---
+++ SmartOrder/app/src/main/java/com/example/smartorder/model/User.java
@@ -0,0 +1,43 @@
+
+package com.example.smartorder.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class User {
+
+    @SerializedName("status")
+    @Expose
+    private String status;
+    @SerializedName("message")
+    @Expose
+    private String message;
+    @SerializedName("user")
+    @Expose
+    private User_ user;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public User_ getUser() {
+        return user;
+    }
+
+    public void setUser(User_ user) {
+        this.user = user;
+    }
+
+}
